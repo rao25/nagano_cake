e40ad18de5dce65211e4dfe72a3003997c3164a7
EC2 Default User <ec2-user@ip-172-31-17-177.ec2.internal>
2022-03-04 07:10:51 +0000

    [Finish] 配送先登録・一覧画面, 配送先編集画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,43 @@
 class Public::AddressesController < ApplicationController
-  def edit
+  
+  def index
+    @address = Address.new
+    @addresses = current_customer.addresses.all
   end
 
-  def index
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path 
+    else
+      @addresses = current_customer.addresses.all
+      render :index
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path 
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+  
+  def address_params
+    params.require(:address).permit(:name,:address,:postal_code)
   end
 end
